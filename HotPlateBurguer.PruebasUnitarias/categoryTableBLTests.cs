504969054020ddb37035a4adb991acb209cc7647
Henry25t <henrycarias96@gmail.com>
2024-10-15 14:23:52 -0600

    Test categoria, food y order

---
+++ HotPlateBurguer.PruebasUnitarias/categoryTableBLTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotPlateRestaurant.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPlateRestaurant.BL.Tests
+{
+    [TestClass()]
+    public class categoryTableBLTests
+    {
+        [TestMethod()]
+        public void CrearAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        public void ObtenerPorIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerTodosAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ HotPlateBurguer.PruebasUnitarias/foodTableBLTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotPlateRestaurant.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPlateRestaurant.BL.Tests
+{
+    [TestClass()]
+    public class foodTableBLTests
+    {
+        [TestMethod()]
+        public void CrearAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerPorIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerTodosAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObtenerTodosPorCategoriasAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
