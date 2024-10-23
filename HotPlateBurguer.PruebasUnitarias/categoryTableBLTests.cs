b5676721eac4535d43971caae08e678290420a49
DouglasBonilla <escobarbonilla27@gmail.com>
2024-10-23 15:55:22 -0600

    prueba unitaria de categoryTable

--- HotPlateBurguer.PruebasUnitarias/categoryTableBLTests.cs
+++ HotPlateBurguer.PruebasUnitarias/categoryTableBLTests.cs
@@ -5,45 +5,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotPlateRestaurant.EN;
 
 namespace HotPlateRestaurant.BL.Tests
 {
     [TestClass()]
     public class categoryTableBLTests
     {
+
+        private static categoryTable categoriaInicial = new categoryTable { Id = 6 };
+        private categoryTableBL categoryTableBL = new categoryTableBL();
         [TestMethod()]
-        public void CrearAsyncTest()
+        public async Task T1CrearAsyncTest()
         {
-            Assert.Fail();
+            var category = new categoryTable();
+            category.Name = "Stake";
+            category.Icon = "Test";
+            int result = await categoryTableBL.CrearAsync(category);
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
-        public void ModificarAsyncTest()
+        public async Task T2ModificarAsyncTest()
         {
-            Assert.Fail();
+            var category = new categoryTable();
+            category.Id = categoriaInicial.Id;
+            category.Name = "Wraps";
+            category.Icon = "Test";
+            int result = await categoryTableBL.ModificarAsync(category);
+            Assert.AreEqual(1, result);
+        }
+
+
+        [TestMethod()]
+        public async Task T3ObtenerPorIdAsyncTest()
+        {
+            var category = new categoryTable();
+            category.Id = categoriaInicial.Id;
+            var result = await categoryTableBL.ObtenerPorIdAsync(category);
+            Assert.AreEqual(category.Id, result.Id);
         }
 
         [TestMethod()]
-        public void DeleteAsyncTest()
+        public async Task T4ObtenerTodosAsyncTest()
         {
-            Assert.Fail();
-        }
-
-        public void ObtenerPorIdAsyncTest()
-        {
-            Assert.Fail();
+            var result = await categoryTableBL.ObtenerTodosAsync();
+            Assert.AreNotEqual(0, result.Count);
         }
 
         [TestMethod()]
-        public void ObtenerTodosAsyncTest()
+        public async Task T5BuscarAsyncTest()
         {
-            Assert.Fail();
+            var category = new categoryTable();
+            category.Name = "Desserts";
+            category.Top_Aux = 10;
+            var resultCategory = await categoryTableBL.BuscarAsync(category);
+            Assert.AreNotEqual(0, resultCategory.Count);
         }
 
         [TestMethod()]
-        public void BuscarAsyncTest()
+        public async Task T6DeleteAsyncTest()
         {
-            Assert.Fail();
+            var category = new categoryTable();
+            category.Id = categoriaInicial.Id;
+            var result = await categoryTableBL.DeleteAsync(category);
+            Assert.AreNotEqual(0, result);
         }
     }
 }
