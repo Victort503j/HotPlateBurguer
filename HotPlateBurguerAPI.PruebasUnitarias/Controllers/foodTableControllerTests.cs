02642c4a6b3ebb939fbbad9eb3f698ec61f0146e
DouglasBonilla <escobarbonilla27@gmail.com>
2024-10-23 15:59:14 -0600

    creacion de clases de las pruebas de integracion de API

---
+++ HotPlateBurguerAPI.PruebasUnitarias/Controllers/foodTableControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotPlateRestaurantAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPlateRestaurantAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class foodTableControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByCategoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
